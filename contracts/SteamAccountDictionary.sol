d4103dbf8796c6c4d62bf104636728747cf68099
Sven Mawby <fr34kyn01535@bam.yt>
2018-01-27 21:45:52 +0100

    Mappings ftw

--- contracts/SteamAccountDictionary.sol
+++ contracts/SteamAccountDictionary.sol
@@ -18,56 +18,25 @@ contract Owned {
     }
 }
 
-contract SteamAccountDictionary is Owned{
-    struct SteamAccount{
-        string steamID64;
-        address walletAddress;
-    }
-    
+contract SteamAccountDictionary is Owned {
     event AddedSteamAccount(string steamID64, address walletAddress);
     event RemovingSteamAccount(string steamID64, address walletAddress);
     
-    SteamAccount[] Dictionary;
+    mapping(string => address) Addresses;
     
     function Add(string _steamID64, address _walletAddress) public onlyOwner 
     {
-        Dictionary.push(SteamAccount(_steamID64,_walletAddress));
+        Addresses[_steamID64] = _walletAddress;
         AddedSteamAccount(_steamID64,_walletAddress);
     }
     
-    function Remove(uint _index) public onlyOwner {
-        RemovingSteamAccount(Dictionary[_index].steamID64,Dictionary[_index].walletAddress);
-        delete Dictionary[_index];
+    function Remove(string _steamID64) public onlyOwner {
+        RemovingSteamAccount(_steamID64,Addresses[_steamID64]);
+        delete Addresses[_steamID64];
     }
     
-    function Get(uint _index) public view returns (string)          
+    function Get(string _steamID64) public view returns (address)          
     {
-        return Dictionary[_index].steamID64;
-    }
-    
-    function GetWalletAddress(string _steamID64) public view returns (address)
-    {
-        uint dictionaryLength = Dictionary.length;
-    
-        for (uint i=0; i< dictionaryLength; i++)
-        {
-            if (keccak256(Dictionary[i].steamID64) == keccak256(_steamID64))
-            {
-                return Dictionary[i].walletAddress;
-            }
-        }
-    }
-    
-    function GetSteamID64(string _walletAddress) public view returns (string)
-    {
-        uint dictionaryLength = Dictionary.length;
-    
-        for (uint i=0; i< dictionaryLength; i++)
-        {
-            if (keccak256(Dictionary[i].walletAddress) == keccak256(_walletAddress))
-            {
-                return Dictionary[i].steamID64;
-            }
-        }
+        return Addresses[_steamID64];
     }
 }
